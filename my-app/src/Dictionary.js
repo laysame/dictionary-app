126542702db3cb4e52c609b628a65e2c932c2609
Laysa Souza <karollinylaysa@gmail.com>
2021-09-30 16:04:18 +0100

    API integration

--- my-app/src/Dictionary.js
+++ my-app/src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState} from "react";
+import axios from "axios";
 import { Button, Col, Form, Row, FormControl, InputGroup} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Dictionary.css";
@@ -6,21 +7,29 @@ import "./Dictionary.css";
 export default function Dictionary() {
     const [keyword, setKeyword] = useState(null);
 
+    function handleResponse(response) {
+        console.log(response.data[0])
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword} meaning`);
+        const apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+console.log(apiUrl)
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function changeSubmittedWord(event){
         setKeyword(event.target.value);
     }
 
+
+
     return (
         <div className="Dictionary">
             <Row className="align-items-center">
             <Form onSubmit={handleSubmit}>
                     <Col className="col-12" xs="auto">
-                        <input type="search" placeholder="Look up for a word..." className="m-2 w-25 Dictionary-form"
+                        <input type="search" placeholder="Look up for a word..." className="m-2 Dictionary-form"
                         onChange={changeSubmittedWord}/>
                         <Button type="submit" className=" m-3 Dictionary-btn">Search</Button>
                     </Col>
